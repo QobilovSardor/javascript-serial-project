767958583b7065913cbc343da82423cbb67b66c5
sardor <sardorqobilov555@gmail.com>
2023-04-13 17:42:06 +0500

    menu card finishd

--- js/script.js
+++ js/script.js
@@ -31,7 +31,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
 		if (target && target.classList.contains('tabheader__item')) {
 			tabheaderItem.forEach((item, idx) => {
-				if(target === item){
+				if (target === item) {
 					hideTabContent();
 					showTabContent(idx);
 				}
@@ -40,41 +40,119 @@ window.addEventListener('DOMContentLoaded', () => {
 	})
 
 	// Loader start
-	const loader = document.querySelector('.loader');
+	// const loader = document.querySelector('.loader');
 
-	setTimeout(() => {
-		loader.style.opacity = '0';
-		setTimeout(() => {
-			loader.classList.add('hide');
-		}, 500)
-	}, 1000);
+	// setTimeout(() => {
+	// 	loader.style.opacity = '0';
+	// 	setTimeout(() => {
+	// 		loader.classList.add('hide');
+	// 	}, 500)
+	// }, 1000);
 
 	// modal start
 	const modal = document.querySelector('.modal'),
 		modalOpen = document.querySelector('[data-modal]'),
 		modalClose = document.querySelector('[data-close]');
 
-		const modalTimer = setTimeout(() => {
+	const modalTimer = setTimeout(() => {
+		showModal();
+	}, 5000)
+	function showModal() {
+		modal.classList.add('show');
+		modal.classList.remove('hide');
+		clearInterval(modalTimer);
+	}
+	function hideModal() {
+		modal.classList.add('hide');
+		modal.classList.remove('show');
+	}
+	modalOpen.addEventListener('click', showModal);
+	modalClose.addEventListener('click', hideModal);
+	modal.addEventListener('click', (e) => {
+		if (modal === e.target) {
+			hideModal();
+		}
+	})
+
+	function showModal() {
+		modal.classList.add('show');
+		modal.classList.remove('hide');
+		clearInterval(modalTimer);
+	}
+
+	function showModalByScroll() {
+		if (window.pageYOffset + document.documentElement.clientHeight > document.documentElement.scrollHeight - 1) {
 			showModal();
-		}, 5000)
-		function showModal() {
-			modal.classList.add('show');
-			modal.classList.remove('hide');
-			clearInterval(modalTimer)
+			window.removeEventListener('scroll', showModalByScroll);
 		}
-		function hideModal() {
-			modal.classList.add('hide');
-			modal.classList.remove('show');
-		}
-		modalOpen.addEventListener('click', showModal);
-		modalClose.addEventListener('click', hideModal);
+	}
+	window.addEventListener('scroll', showModalByScroll);
 
-		modal.addEventListener('click', (e) => {
-			if(e.target === modal) {
-				hideModal();
+	// Class
+
+	class MenuCard {
+		constructor(img, alt, title, descr, price, parenElement, ...classes) {
+			this.img = img,
+			this.alt = alt,
+			this.title = title,
+			this.descr = descr,
+			this.price = price,
+			this.transfer = 11000,
+			this.parent = document.querySelector(parenElement),
+			this.classes = classes,
+			this.changeToUSZ()
+		}
+
+		changeToUSZ() {
+			this.price = this.price * this.transfer
+		}
+
+		render() {
+			const element = document.createElement('div');
+
+			if (this.classes.length === 0) {
+				this.element = 'menu__item';
+				element.classList.add(this.element);
+			} else{ 
+				this.classes.forEach(className => element.classList.add(className))
 			}
-		})
 
-		// Date
+			element.innerHTML = `
+				<img src=${this.img} alt=${this.alt} />
+				<h3 class="menu__item-subtitle">${this.title}</h3>
+				<div class="menu__item-descr">${this.descr}</div>
+				<div class="menu__item-divider"></div>
+				<div class="menu__item-price">
+					<div class="menu__item-cost">Price:</div>
+					<div class="menu__item-total"><span>${this.price}</span> month</div>
+				</div>
+			`
+			this.parent.append(element)
+		}
+	}
 
+	new MenuCard(
+		'img/tabs/1.png',
+		'vegy',
+		'Plan "Usual"',
+		'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit nesciunt facere, sequi exercitationem praesentium ab cupiditate beatae debitis perspiciatis itaque quaerat id modi corporis delectus ratione nobis harum voluptatum in.',
+		10,
+		'.menu .container'
+	).render()
+	new MenuCard(
+		'img/tabs/2.jpg',
+		'elite',
+		'Plan “Premium”',
+		'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit nesciunt facere, sequi exercitationem praesentium ab cupiditate beatae debitis perspiciatis itaque quaerat id modi corporis delectus ratione nobis harum voluptatum in.',
+		15,
+		'.menu .container'
+	).render()
+	new MenuCard(
+		'img/tabs/3.jpg',
+		'post',
+		'Plan "VIP"',
+		'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit nesciunt facere, sequi exercitationem praesentium ab cupiditate beatae debitis perspiciatis itaque quaerat id modi corporis delectus ratione nobis harum voluptatum in.',
+		20,
+		'.menu .container'
+	).render()
 })
